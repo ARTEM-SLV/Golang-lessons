825d7379fdab2073464424f94881b206fe236e7b
Artem <67641352+ARTEM-SLV@users.noreply.github.com>
2024-04-08 12:26:15 +0300

    Lesson 5.4

--- Lesson 5/5.4/main.go
+++ Lesson 5/5.4/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	var s string
-	s = "Hello, Go"
+	var (
+		num     int     = 42
+		str     string  = "Hello"
+		flt     float64 = 3.14
+		boolean bool    = true
+	)
 
-	fmt.Println(s)
-	fmt.Println(&s)
+	fmt.Printf("num:   value = %d, address = %p\n", num, &num)
+	fmt.Printf("str:   value = %s, address = %p\n", str, &str)
+	fmt.Printf("flt:   value = %f, address = %p\n", flt, &flt)
+	fmt.Printf("array: value = %v, address = %p\n", boolean, &boolean)
+
+	// Размер переменных в байтах
+	fmt.Printf("Size of int:     %d bytes\n", unsafe.Sizeof(num))
+	fmt.Printf("Size of string:  %d bytes\n", unsafe.Sizeof(str))
+	fmt.Printf("Size of float64: %d bytes\n", unsafe.Sizeof(flt))
+	fmt.Printf("Size of array:   %d bytes\n", unsafe.Sizeof(boolean))
 }

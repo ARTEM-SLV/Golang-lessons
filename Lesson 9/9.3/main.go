6a5c84144ff8f143d7f5398bd330f3c2117873d4
Artem <arty.slv1984@gmail.com>
2024-05-01 11:56:36 +0300

    Lesson 9.3

--- Lesson 9/9.3/main.go
+++ Lesson 9/9.3/main.go
@@ -7,19 +7,11 @@ func main() {
 	checkFood(food)
 }
 
-func checkFood(food string) {
-	switch food {
-	case "груша":
+func checkFood(f string) {
+	switch {
+	case f == "груша" || f == "яблоко" || f == "апельсин":
 		fmt.Println("это фрукт")
-	case "яблоко":
-		fmt.Println("это фрукт")
-	case "апельсин":
-		fmt.Println("это фрукт")
-	case "тыква":
-		fmt.Println("это овощ")
-	case "огурец":
-		fmt.Println("это овощ")
-	case "помидор":
+	case f == "тыква" || f == "огурец" || f == "помидор":
 		fmt.Println("это овощ")
 	default:
 		fmt.Println("что-то странное...")

40e967deb2e88560abf6596d40ee9957dc95ebfe
Artem <arty.slv1984@gmail.com>
2024-05-01 16:17:32 +0300

    Lesson 10.4

---
+++ Lesson_10/10.4/go.mod
@@ -0,0 +1,7 @@
+module lesson
+
+go 1.22.1
+
+require github.com/ARTEM-SLV/module v1.0.0
+
+require github.com/ARTEM-SLV/module/v2 v2.0.0
---
+++ Lesson_10/10.4/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	v1 "github.com/ARTEM-SLV/module"
+	v2 "github.com/ARTEM-SLV/module/v2"
+)
+
+func main() {
+	v1.Hello()
+	v2.Hello()
+}

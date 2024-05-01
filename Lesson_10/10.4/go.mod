1726e9118f4c359d2bcddfd0d4dd89fb498ed256
Artem <arty.slv1984@gmail.com>
2024-05-01 14:49:16 +0300

    del

--- Lesson_10/10.4/go.mod
+++
@@ -1,5 +0,0 @@
-module lesson
-
-go 1.22.1
-
-require github.com/ARTEM-SLV/go_module v1.0.0 // indirect
--- Lesson_10/10.4/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ARTEM-SLV/go_module"
-
-func main() {
-	go_module.Hello()
-}

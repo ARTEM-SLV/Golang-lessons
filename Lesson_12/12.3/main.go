9eefa05ed6470bdf0f6cd48f4f9236fb95cb6c84
Arty <arty.slv1984@gmail.com>
2024-05-06 11:02:27 +0300

    lesson 12.3

---
+++ Lesson_12/12.3/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type format interface {
+	Format()
+}
+
+type Xml struct{}
+
+func (x Xml) Format() {
+	fmt.Println("Данные в формате xml")
+}
+
+type Csv struct{}
+
+func (c Csv) Format() {
+	fmt.Println("Данные в формате csv")
+}
+func main() {
+	x := Xml{}
+	Report(x)
+	c := Csv{}
+	Report(c)
+}
+func Report(f format) {
+	f.Format()
+}

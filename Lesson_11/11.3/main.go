2ed8c280530079c227ae66767d7e8e95c864a2c5
Artem <arty.slv1984@gmail.com>
2024-05-06 09:55:53 +0300

    Lesson 11.3

--- Lesson_11/11.3/main.go
+++ Lesson_11/11.3/main.go
@@ -5,49 +5,13 @@ import (
 	"fmt"
 )
 
-type ErrOne error
-type ErrTwo error
-
-func (e ErrOne) Error() string {
-	return "ошибка1"
-}
-
-//func (e ErrTwo) Error() string {
-//	return "ошибка2"
-//}
-
 func main() {
-	var err ErrOne
-	err := errors.New("")
+	err1 := errors.New("ошибка1")
+	err2 := fmt.Errorf("ошибка2:%w", err1)
+	err3 := fmt.Errorf("ошибка3:%w", err2)
+	fmt.Println(err3)
 
-	fmt.Println(err.Error())
+	if errors.Is(err3, err1) {
+		fmt.Println(err1)
+	}
 }
-
-//func checkInt1(v int) error {
-//	var errData errOne
-//	errData.err = checkInt2(v)
-//	if errData.err != nil {
-//		return fmt.Errorf("ошибка1:%w", errData.err)
-//	}
-//
-//	return nil
-//}
-//
-//func checkInt2(v int) error {
-//	var errData errTwo
-//
-//	errData.err = checkInt3(v)
-//	if errData.err != nil {
-//		return fmt.Errorf("ошибка2:%w", errData.err)
-//	}
-//
-//	return nil
-//}
-//
-//func checkInt3(v int) error {
-//	if v < 0 {
-//		return errors.New("ошибка3")
-//	}
-//
-//	return nil
-//}

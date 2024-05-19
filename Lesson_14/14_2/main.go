7739e0fe3327867230aa4d1be9acd53aef7d467a
Artem <arty.slv1984@gmail.com>
2024-05-19 15:06:02 +0300

    14.3

--- Lesson_14/14_2/main.go
+++ Lesson_14/14_2/main.go
@@ -1,10 +1,3 @@
-/*
-Create an unbuffered channel that will
-accept a string value. Write “Hello,
-string channel!” to the channel,
-then read this value and output to stdout
-*/
-
 package main
 
 import "fmt"

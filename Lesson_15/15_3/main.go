a606119b8ca37488f8105fe72f25c00643e5045b
Artem <arty.slv1984@gmail.com>
2024-05-20 12:47:33 +0300

    15.3

---
+++ Lesson_15/15_3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var n int
+	var mu sync.Mutex
+	for i := 0; i < 10; i++ {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			n++
+		}()
+	}
+
+	time.Sleep(2 * time.Millisecond)
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Println(n)
+}

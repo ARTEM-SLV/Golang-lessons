28ed2ad112aefa76d080bc3f85bd2d79caf883e4
Artem <arty.slv1984@gmail.com>
2024-05-20 12:26:39 +0300

    15.1

--- Lesson_15/15_1/main.go
+++ Lesson_15/15_1/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("горутина:", i+1)
+		}()
+	}
+	wg.Wait()
 }
